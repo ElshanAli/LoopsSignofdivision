0b20c4a3e51b734a29941b98947f862459103133
ElshanAli <elshanml@code.edu.az>
2022-04-18 17:31:25 +0400

    Added task Sign of Division

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SignofdivisionLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the numbers");
+            int n = Convert.ToInt32(Console.ReadLine());
+            if (n%7==0)
+            {
+                Console.WriteLine("The number is divided by 7");
+            }
+            else if (n%7<=3)
+            {
+               
+                Console.WriteLine("The nearest number divisible by 7:" + " " + (n- n%7));
+            }
+            else
+            {
+                Console.WriteLine("The nearest number divisible by 7:" + " " + (n +7-n%7));
+                
+            }
+
+
+
+        }
+    }
+}
